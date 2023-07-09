d3a3165924cb9908cd76ad432300fec85157c0d2
Goh Shang Shein <VegeGrinder@gmail.com>
2023-07-09 20:43:29 +0800

    Updated title

--- resources/views/layout/default.blade.php
+++ resources/views/layout/default.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel</title>
+        <title>Replacement Scheduler</title>
 
         @vite(['resources/sass/app.scss','resources/js/app.js'])
         @stack('css')
